cccb1eec25f67b2a3308890af0178f65d0ab3bfc
metamn <bartus.csongor@gmail.com>
2013-11-20 10:56:26 +0200

    The barebones Anvelocom theme added

---
+++ wp-content/themes/anvelocom/footer.php
@@ -0,0 +1,20 @@
+<footer id="footer">
+</footer>
+
+
+
+<!-- jQuery from CDN and local fallback -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/jquery-1.10.2.min.js">\x3C/script>')</script>
+
+<!-- jQuery Isotope plugin -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/jquery.isotope.min.js"></script>
+
+<!-- Site specific scripts built on jQuery -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/jquery.reboot.js"></script>
+ 
+ 
+   
+</div>
+</body>
+</html>
