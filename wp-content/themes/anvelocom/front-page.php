54ccd69978c203b20e9c8a6cb03e33ca86c50315
metamn <bartus.csongor@gmail.com>
2013-11-20 19:33:51 +0200

    Frontpage starting to look good

--- wp-content/themes/anvelocom/front-page.php
+++ wp-content/themes/anvelocom/front-page.php
@@ -9,17 +9,59 @@
       include '_responsive-images.php';
     ?>
     <figcaption>
-      <?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?>
+      <a href=""><?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?></a>
     </figcaption>
   </figure>
 </section>
 
 <section id="anvelope">
-  <h2>Anvelope noi</h2>
   <?php 
-    $anvelopes = get_anvelopes(10);  
-    if ($anvelopes) {
-      foreach ($anvelopes as $index=>$article) { ?>
+    $anvelopes = get_anvelopes(4);  
+    if ($anvelopes) { ?>
+      <article class="thumb c0">
+        <h1><a href="">Produse noi</a></h1>
+      </article>
+      
+      <?php foreach ($anvelopes as $index=>$article) { ?>
+      <article class="thumb c<?php echo $index + 1 ?>">
+        <?php 
+          $image_size = 'medium';
+          include '_article.php';
+        ?>
+     </article>
+    <?php } ?>
+  <?php } ?>
+</section>
+
+<section id="reduceri">
+  <?php 
+    $anvelopes = get_anvelopes(4);  
+    if ($anvelopes) { ?>
+      <article class="thumb c0">
+        <h1><a href="">Reduceri</a></h1>
+      </article>
+      
+      <?php foreach ($anvelopes as $index=>$article) { ?>
+      <article class="thumb c<?php echo $index + 1 ?>">
+        <?php 
+          $image_size = 'medium';
+          include '_article.php';
+        ?>
+     </article>
+    <?php } ?>
+  <?php } ?>
+</section>
+
+
+<section id="bestsellers">
+  <?php 
+    $anvelopes = get_anvelopes(4);  
+    if ($anvelopes) { ?>
+      <article class="thumb c0">
+        <h1><a href="">Cele mai vandute</a></h1>
+      </article>
+      
+      <?php foreach ($anvelopes as $index=>$article) { ?>
       <article class="thumb c<?php echo $index + 1 ?>">
         <?php 
           $image_size = 'medium';
