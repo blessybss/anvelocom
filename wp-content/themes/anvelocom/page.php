6bf708e44e0dd71632cde95651f444f4d26439c2
metamn <bartus.csongor@gmail.com>
2013-11-21 12:13:54 +0200

    Single page content done

--- wp-content/themes/anvelocom/page.php
+++ wp-content/themes/anvelocom/page.php
@@ -1,15 +1,40 @@
 <?php get_header(); ?>
 
+<section id="page">
   <?php 
     if (have_posts()) {
       while ( have_posts() ) : the_post(); ?>
         
         page
         
+        <?php
+	        $mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
+
+	        foreach( $mypages as $page ) {		
+		        $content = $page->post_content;
+		        if ( ! $content ) // Check for empty page
+			        continue;
+
+		        $content = apply_filters( 'the_content', $content );
+	        ?>
+		        <h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
+		        <div class="entry"><?php echo $content; ?></div>
+	        <?php
+	        }	
+        ?>
+        
       <?php endwhile;
 		} else {
 		  include '_not-found.php';
 		}
 	?>
+</section>
+
+<section id="sidebar">
+  <?php wp_list_categories(array(
+    'title_li' => '',
+    'exclude' => '467',
+  )); ?>  
+</section>
 
 <?php get_footer(); ?>
